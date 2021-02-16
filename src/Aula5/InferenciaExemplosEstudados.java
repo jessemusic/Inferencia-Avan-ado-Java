96beb0c9e2ddd83a930944e4a96b3c5e39c2fd20
jessemusic <jmc_cursos@hotmail.com>
2021-02-15 23:36:27 -0300

    Utilizando var como inferencia em novos releases em java

--- src/Aula5/InferenciaExemplosEstudados.java
+++ src/Aula5/InferenciaExemplosEstudados.java
@@ -4,14 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.stream.Collectors;
 
 public class InferenciaExemplosEstudados {
     public static void main(String[] args) throws IOException {
-        URL url = new URL( "https://docs.oracle.com/javase/10/language/" );
-        URLConnection urlConnection = url.openConnection();
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( urlConnection.getInputStream()) );
+        var url = new URL( "https://docs.oracle.com/javase/10/language/" );
+        var urlConnection = url.openConnection();
+        var bufferedReader = new BufferedReader( new InputStreamReader( urlConnection.getInputStream()) );
 
         System.out.println(bufferedReader.lines().collect( Collectors.joining())
                 .replaceAll( ">",">\n" ));
